da5fe3de4acf35f89b190e597cb747bec05cf23e
vishal13d <vishal.dhakad@nammayatri.in>
2025-07-03 19:31:01 +0530

    addfleet

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import utils.ToastMessageHandler;
 
 public class LoginPage{
 
     private WebDriver driver;
+    
 
 private By oparatormobileNumber = By.xpath("//input[@placeholder='Enter mobile no.']");
 private By checkBox = By.xpath("//input[@type='checkbox']");
@@ -28,9 +30,11 @@ this.driver=driver;
 
 
 public void loginAsOperator(String MobileNo, String otp){
+    ToastMessageHandler toastHandler = new ToastMessageHandler(driver);
     driver.findElement(oparatormobileNumber).sendKeys(MobileNo);
     driver.findElement(checkBox).click();
     driver.findElement(continueButton).click();
+    if (toastHandler.checkAndPrintToastError()) return;
 
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     wait.until(ExpectedConditions.visibilityOfElementLocated(otpFields));
